9cc5b6fc4f3c17be3cb835a53bc97b807de76d2b
Destino Dello <destinodello@gmail.com>
2023-11-16 16:15:18 +0000

    v1 demo

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px"
+    },
+    extend: {
+      keyframes: {
+        move: {
+          "50%": {transform: 'translateY(-1rem)'}
+        }
+      },
+      animation: {
+        'movingY': 'move 2s linear infinite'
+      }
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '1.5rem'
+      }
+    },
+    fontFamily: {
+      inter: ['Inter', 'sans-serif'],
+      dmsans: ['DM Sans', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
